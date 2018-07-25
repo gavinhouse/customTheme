50e571f60c9ab09c7164fc761c743e9c204a1631
Gavin <gavin.house@ttu.edu>
2018-07-25 12:53:03 +0100

    display thumbnails in post listing

--- content.php
+++ content.php
@@ -2,8 +2,18 @@
     <h2 class="blog-post-title"><?php the_title(); ?></h2>
     <p class="blog-post-meta"><?php the_date(); ?> by <?php the_author(); ?></p>
 
-    <?php the_excerpt(); ?>
-
+    <?php if ( has_post_thumbnail() ) {?>
+        <div class="row">
+            <div class="col-md-4">
+                <?php	the_post_thumbnail('thumbnail'); ?>
+            </div>
+            <div class="col-md-6">
+                <?php the_excerpt(); ?>
+            </div>
+        </div>
+    <?php } else { ?>
+        <?php the_excerpt(); ?>
+    <?php } ?>
     <form action="<?php the_permalink(); ?>">
         <input class="btn btn-dark" type="submit" value="Read More" />
     </form>
