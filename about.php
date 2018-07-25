041c3121f67ee746ebb3e2a9ee3bb7db1677c038
Gavin <gavin.house@ttu.edu>
2018-07-25 10:00:21 +0100

    added file for about page

---
+++ about.php
@@ -0,0 +1,12 @@
+<div class="about-page">
+    <div class="about-header">
+        <h2 class="about-title"><?php the_title(); ?></h2>
+    </div>
+    <div class="about-content">
+        <?php if (has_post_thumbnail()){
+            the_post_thumbnail('single-image');
+            echo '<br /><br />';
+        } ?>
+        <?php the_content(); ?>
+    </div>
+</div>
--- page.php
+++ page.php
@@ -6,7 +6,12 @@
             <?php
             if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-                get_template_part( 'content-single', get_post_format() );
+                if($pagename === 'about'){
+                    get_template_part( 'about', get_post_format() );
+                }
+                else{
+                    get_template_part( 'content-single', get_post_format() );
+                }
 
             endwhile; endif;
             ?>
